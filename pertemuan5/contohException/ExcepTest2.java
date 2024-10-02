ecb416e522e4dbc3d2f383754369fb643457f5c2
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-02 15:54:42 +0700

    Update ExcepTest2.java

--- pertemuan5/contohException/ExcepTest2.java
+++ pertemuan5/contohException/ExcepTest2.java
@@ -5,12 +5,12 @@ public class ExcepTest2 {
         int a[]=new int[2];
         int b=0;
 
-        try {
+        try { //mencoba membagi c dengan 0 dan mengakses array a pada indeks ke-3
             int c=1/b;
             System.out.println("access element three: "+a[3]);
-        } catch(ArrayIndexOutOfBoundsException ex){
+        } catch(ArrayIndexOutOfBoundsException ex){ //mencegah kesalahan permintaan array menghentikan program
             System.out.println("ArrayIndexOutOfBondsException thrown: "+ex);
-        }catch (Exception ex) {
+        }catch (Exception ex) { //mencegah kesalahan secara umum terjadi, artinya dalam kasus ini mencegah kesalahan pembagian angka
             System.out.println("Exception thrown: "+ex);
         }
         System.out.println("Out of block.");
