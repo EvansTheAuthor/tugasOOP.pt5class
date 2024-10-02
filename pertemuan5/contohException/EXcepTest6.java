fd909efc4d3093984b8b748e4413d5626f7f32c6
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-02 16:01:27 +0700

    Update EXcepTest6.java

--- pertemuan5/contohException/EXcepTest6.java
+++ pertemuan5/contohException/EXcepTest6.java
@@ -3,11 +3,11 @@ package pertemuan5.contohException;
 public class EXcepTest6 {
     public static void main(String[] args) {
         int a[]=new int[2];
-        try {
+        try { //mencoba mengakses array pada indeks ke-3
             System.out.println("Access element three: "+a[3]);
-        } catch (ArithmeticException e) {
+        } catch (ArithmeticException e) { //mencegah kesalahan perhitungan namun karena tidak ada jadi tidak akan ada error spesifik yang akan diberitahukan
             System.out.println("Exception thrown --> "+e);
-        }finally{
+        }finally{ //mengatur isi indeks ke-0 adalah 6
             a[0]=6;
             System.out.println("First element value: "+a[0]);
             System.out.println("The finally statement is executed");
