3383bc79df0ca6b8110c40e190d0f2b698d7d14b
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-02 15:19:43 +0700

    Update uncheckedDemo.java

--- pertemuan5/contohException/uncheckedDemo.java
+++ pertemuan5/contohException/uncheckedDemo.java
@@ -4,9 +4,9 @@ public class uncheckedDemo {
     public static void main(String[] args) {
         int num[]={1,2,3,4};
         
-        try{
+        try{ //mencoba menjalankan num pada indeks ke-5
             System.out.println(num[5]);
-        }catch(ArrayIndexOutOfBoundsException ex){
+        }catch(ArrayIndexOutOfBoundsException ex){ //mencegah program berhenti dengan menunjukkan masalah yang terjadi
             System.out.println("There is an error"+ex.getMessage());
         }finally{System.out.println("Process is end.");}
     }
