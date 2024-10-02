6c2843fe176fe157743fad69ba3f82063fde2cb2
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-02 15:58:49 +0700

    Update ExcepCaseStudy.java

--- pertemuan5/contohException/ExcepCaseStudy.java
+++ pertemuan5/contohException/ExcepCaseStudy.java
@@ -2,11 +2,11 @@ package pertemuan5.contohException;
 
 public class ExcepCaseStudy {
     public static void main(String[] args) {
-        try {
+        try { //mencoba mengisi indeks ke-5 dengan 100 padahal indeks maksimal adalah 4
             int a[]=new int[5];
             a[5]=100;
             //System.out.println("Access the variable: "+a[4]);
-        } catch (Exception e) {
+        } catch (Exception e) { //menampilkan error yang terjadi dan mencegah program berhenti
             System.out.println("Caught an exception --> "+e);
         }
     }
