bef74b52a0df86d0723382da6402bf70824c731b
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-02 15:55:23 +0700

    Update ExcepTest.java

--- pertemuan5/contohException/ExcepTest.java
+++ pertemuan5/contohException/ExcepTest.java
@@ -2,10 +2,10 @@ package pertemuan5.contohException;
 
 public class ExcepTest {
     public static void main(String[] args) {
-        try {
+        try { //mencoba mengakses array a pada indeks ke-3
             int a[]=new int[2];
             System.out.println("Access element three: "+a[3]);
-        } catch (ArrayIndexOutOfBoundsException ex) {
+        } catch (ArrayIndexOutOfBoundsException ex) { //mencegah permintaan akses array yang salah menghentikan program
             System.out.println("Exception thrown: "+ex);
         }
         System.out.println("Out of the block");
