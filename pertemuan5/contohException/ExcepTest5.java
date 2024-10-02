0614f769048be62a1d6aee0093e96f639973bd83
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-02 15:26:48 +0700

    Update ExcepTest5.java

--- pertemuan5/contohException/ExcepTest5.java
+++ pertemuan5/contohException/ExcepTest5.java
@@ -2,17 +2,17 @@ package pertemuan5.contohException;
 
 public class ExcepTest5 {
     public static void main(String[] args) {
-        try {
+        try { //mencoba mengakses array a pada indeks ke-3
             int a[]=new int[2];
 
-            try {
+            try { //mencoba membagi c dengan 0 alias b
                 int b=0;
                 int c=1/b;
-            } catch (Exception e) {
+            } catch (Exception e) { //mencegah error menghentikan program dengan menampilkan spesifik masalah
                 System.out.println("Exception thrown: "+e);
             }
             System.out.println("Access element three: "+a[3]);
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) { //mencegah error menghentikan program dengan memberitahu bahwa array tidak bisa diakses dengan indeks yang diinginkan
             System.out.println("Exception thrown: "+e);
         }
         System.out.println("Out of the block");
