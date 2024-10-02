3eacbff8a088b2f3c7695f328ea45052943c781b
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-10-02 15:31:42 +0700

    Update ExcepTest4.java

--- pertemuan5/contohException/ExcepTest4.java
+++ pertemuan5/contohException/ExcepTest4.java
@@ -12,17 +12,17 @@ public class ExcepTest4 {
         System.out.print("Enter value b: ");
         b=scanner.nextInt();
         
-        try {
+        try { //mencoba mengurangi a dengan nilai b berdasar nilai yang diinput user
             int result=a-b;
             if (result<0) {
                 throw new IllegalArgumentException("the result may be negative");
             }
             System.out.println("Hasil: "+result);
-        } catch(IllegalArgumentException e){
+        } catch(IllegalArgumentException e){ //mencegah hasil bilangan negatif keluar
             System.out.println("Exception: "+e.getMessage());
-        }catch (Exception e) {
+        }catch (Exception e) {// ada sebab masalah lain
             System.out.println("The result cannot  be processed");
-        }finally{
+        }finally{//menjalankan proses tanpa pengaruh try-catch
             System.out.println();
             System.out.println("Process is end");
         }
